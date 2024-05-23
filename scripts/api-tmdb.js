47c39030d9aa5f776829ae5f74f2bd5c066bd264
m0nt4ld0 <mmontaldo@live.com>
2024-05-23 15:34:28 -0300

    Hace busqueda a traves del campo de texto de busqueda haciendo clic en el boton y muestra resultados con resumen hasta 147 caracteres x tarjeta

--- scripts/api-tmdb.js
+++ scripts/api-tmdb.js
@@ -1,8 +1,15 @@
 const apiKey = 'c1f057151eae68909df52e38dfb7c19c';
-const apiUrl = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`;
+//const apiUrl = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`;
 const outputElement = document.getElementById('salidaAPI');
+const searchElement = document.getElementById('txtBusqueda').innerText;
+const titleElement = document.getElementsByTagName('title')[0];
 
-fetch(apiUrl)
+function busqueda() {
+    const searchFilm = document.getElementById('txtBusqueda').value;
+    //console.log(searchFilm);
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${searchFilm}&api_key=${apiKey}`;
+    //console.log(apiUrl);
+    fetch(apiUrl)
   .then(response => {
     if (!response.ok) {
       throw new Error('Error de red');
@@ -10,16 +17,22 @@ fetch(apiUrl)
     return response.json();
   })
   .then(data => {
-    outputElement.innerHTML = `
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${data.poster_path}" alt="${data.original_title}">
-        <div class="card-body">
-        <h5 class="card-title">${data.original_title}</h5>
-        <p class="card-text">${data.overview}</p>
-        <a href="#" class="btn btn-primary">Ver más</a>
-        </div>
-    </div>`
+    for(var r of data.results) {
+        titleElement.innerHTML = `CAC-MOVIES | Resultados`,
+        outputElement.innerHTML += `
+        <div class="card" style="width: 10rem; float:left; margin-left:2rem;">
+            <img class="card-img-top" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${r.poster_path}" alt="${r.original_title}">
+            <div class="card-body">
+            <h5 class="card-title">${r.original_title}</h5>
+            <p class="card-text">${r.overview.substr(0,147)}...</p>
+            <a href="#" class="btn btn-primary">Ver más</a>
+            </div>
+        </div>`
+        
+    }
+    
   })
   .catch(error => {
     console.error('Error:', error);
   });
+}
