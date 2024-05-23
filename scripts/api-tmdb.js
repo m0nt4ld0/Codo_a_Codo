59da839f52c879c95c2b356ca21108f93fe54a12
m0nt4ld0 <mmontaldo@live.com>
2024-05-23 16:06:19 -0300

    Quita la API KEY del codigo

--- scripts/api-tmdb.js
+++ scripts/api-tmdb.js
@@ -1,5 +1,4 @@
-const apiKey = 'c1f057151eae68909df52e38dfb7c19c';
-//const apiUrl = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`;
+const apiKey = '';
 const outputElement = document.getElementById('salidaAPI');
 const searchElement = document.getElementById('txtBusqueda').innerText;
 const titleElement = document.getElementsByTagName('title')[0];
@@ -19,7 +18,7 @@ function busqueda() {
     for(var r of data.results) {
         titleElement.innerHTML = `CAC-MOVIES | ${cant} Resultados para "${searchFilm}"`,
         outputElement.innerHTML += `
-        <div class="card" style="width: 10rem; float:left; margin-left:2rem;">
+        <div class="card" style="width: 10rem; float:left; margin:2rem;">
             <img class="card-img-top" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${r.poster_path}" alt="${r.original_title}">
             <div class="card-body">
             <h5 class="card-title">${r.original_title}</h5>
